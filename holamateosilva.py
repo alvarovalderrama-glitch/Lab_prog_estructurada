ba6ce74f78baec89b746ac303612c87810c8e498
MateoSilva999 <mateo.silva@userena.cl>
2025-10-06 18:59:39 -0300

    Initial commit

---
+++ holamateosilva.py
@@ -0,0 +1,9 @@
+spam = int(input('Ingresa un número: '))
+if spam == 1 :
+    print('Hola')
+elif spam == 2 :
+    print('Hola')
+elif spam < 1 :
+    print('¡Saludos!')
+elif spam > 2 :
+    print('¡Saludos!')
