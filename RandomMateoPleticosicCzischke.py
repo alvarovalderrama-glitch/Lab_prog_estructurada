9aa1f4efe28d8fa39e2a58ad988999a469db635f
MateoPleticosic <mateo.pleticosic@userena.cl>
2025-10-13 18:18:25 -0300

    Add files via upload

---
+++ RandomMateoPleticosicCzischke.py
@@ -0,0 +1,28 @@
+import random
+random_number = random.randint(1, 6)
+
+def get_random_dice_roll():
+  # Returns a random integer from 1 to 6
+  return random_number
+
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+
+
+print("---------------Solución-------------------------------")
+
+#Como solucionar el Problema
+
+import random
+def get_random_dice_roll():
+  # Returns a random integer from 1 to 6
+  random_number = random.randint(1, 6)
+  return random_number
+
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+
