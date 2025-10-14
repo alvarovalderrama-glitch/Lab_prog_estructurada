75c07c1a62d27d43c15315f7824485ddb8f3d9ec
ignaciotapiav-star <ignacio.tapiav@userena.cl>
2025-10-14 17:24:06 -0300

    Add files via upload

--- randomignaciotapia.py
+++ randomignaciotapia.py
@@ -2,9 +2,12 @@ import random
 random_number = random.randint(1, 6)
 
 def get_random_dice_roll():
-    #returns a rantom integer from 1 to random_number
+    random_number = random.randint(1, 6)
+    return random_number
+    
 
-    print(get_random_dice_roll())
-    print(get_random_dice_roll())
-    print(get_random_dice_roll())
-    print(get_random_dice_roll())
+
+print (get_random_dice_roll())
+print (get_random_dice_roll())
+print (get_random_dice_roll())
+print (get_random_dice_roll())
