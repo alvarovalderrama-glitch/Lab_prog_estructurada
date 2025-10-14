27a7f1cb6b27dd0a3ddb7de60cd114063f574372
Julian-DiazR <julian.diazr@userena.cl>
2025-10-14 17:05:05 -0300

    Update and rename randomsJulianDiaz.py to randomJulianDiaz.py

--- randomJulianDiaz.py
+++ randomJulianDiaz.py
@@ -8,6 +8,8 @@ def get_random_dice_roll():
 print(get_random_dice_roll())
 print(get_random_dice_roll())
 print(get_random_dice_roll())
+print(get_random_dice_roll())
 
 # Este programa define un número aleatorio solo una vez (random_number) 
 # y cada llamada a la función devuelve siempre ese mismo valor.
+
