d4927ecaa1c40bfb363c7e330fc22ef5ba845be2
valen <valentina.arancibiac@userena.cl>
2025-10-14 16:38:10 -0300

    eva 1

---
+++ holavalentinaarancibia.py
@@ -0,0 +1,10 @@
+spam = int(input("ingrese un numero: "))
+if spam == 1:
+    print('Hola')
+elif spam > 1:
+    if spam == 2:
+       print('Hola')
+    else:
+        print('¡Saludos!')
+else:
+    print('Saludos!')
