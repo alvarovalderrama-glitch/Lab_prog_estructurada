208e53d34829608571dc14a7a270a3c125cb9444
maximilianoangel-debug <maximiliano.angel@userena.cl>
2025-10-14 17:08:00 -0300

    Update randommaximilianoangel.py

--- randommaximilianoangel.py
+++ randommaximilianoangel.py
@@ -20,28 +20,40 @@ def random_sin_repetidos(): # Define la función con el nombre "random_sin_repet
     print(random.randint(1, 6)) # esta,
     print(random.randint(1, 6)) # y esta.
 
+def random_sin_repetidos2(): # Define la función con el nombre "random_sin_repetidos2".
+    
+    def get_random_dice_roll(): # Define una función con el nombre "get_random_dice_roll".
+        random_number = random.randint(1, 6) # Se le asigna a la varible "random_number" un número aleatorio entre 1 y 6.
+        return random_number # Nos regresa / da como resultado la variable "random_number".
+
+    print(get_random_dice_roll()) # Imprime lo resultante de la función "get_random_dice_roll2" dando números aleatorios.
+    print(get_random_dice_roll())
+    print(get_random_dice_roll())
+    print(get_random_dice_roll())
+
 ### Main / Menú ###
 
 while True: # Ciclo repetivo "while" con el valor booleano True, para que se repita indefinidamente.
     # Se imprime el menú para poder ver las opciones que se pueden seleccionar.
     print("""
-|============Menú de números============|
-     1.- Todos los números iguales.
-     2.- Todos números aleatorios.
-     3.- Salir.
-|=======================================|
+|================Menú de números================|
+     1.- Todos los números iguales (Original).
+     2.- Todos números aleatorios (Opción 1).
+     3.- Todos números aleatorios (Opción 2).
+     4.- Salir.
+|===============================================|
  """)
     # Se crea la variable "seleccion" en la que se usa un "input(...)", para que el usuario ingrese que opción quiere y que la tome como una cadena de texto (string).
     seleccion = input("Ingrese el número de la acción que desea: \n> ")
-    if seleccion == "1": # Condicional "if" que ejecutará la función "random_repetido" si (seleccion == "1") 
+    print(" ")
+    if seleccion == "1": # Condicional "if" que ejecutará la función "random_repetido" si "seleccion" es igual a "1". 
         random_repetido() # Llama a la función "random_repetido".
-    elif seleccion == "2": # Condicional "elif" que ejecutará la función "random_sin_repetidos" si (seleccion == "2") 
+    elif seleccion == "2": # Condicional "elif" que ejecutará la función "random_sin_repetidos" si "seleccion" es igual a "2".
         random_sin_repetidos() # Llama a la función "random_sin_repetidos".
-    elif seleccion == "3": # Condicional "elif" que imprimirá un mensaje y hará que termine el ciclo "while", si (seleccion == "3") .
+    elif seleccion == "3": # Condicional "elif" que ejecutará lo que esta en su interior si "seleccion" es igual a "3"
+        random_sin_repetidos2() # Llama a la función "random_sin_repetidos2".
+    elif seleccion == "4": # Condicional "elif" que imprimirá un mensaje y hará que termine el ciclo "while", si "seleccion" es igual a "4".
         print("Saliendo del programa...") # Imprime el mensaje "Saliendo del programa...".
         break # Hace que se rompa el ciclo "while".
     else: # Si no se cumple ninguna de las condiciones anteriores ejecutará lo siguiente.
         print("Opción invalida, elige otra.") # Imprime el mensaje "Opción invalida, elige otra."
-
-    
-
