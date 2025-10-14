e47b90f8f67dc84854c0907ffe971b0fdcda0095
Pablo-va <pablo.vegaa@userena.cl>
2025-10-14 18:52:21 -0300

    Add files via upload

---
+++ mmpablovega.py
@@ -0,0 +1,24 @@
+def mm(A, B):
+
+    BT = list(zip(*B))  
+
+    C = []
+    for fila in A:                 
+        nueva_fila = []
+        for col in BT:               
+            valor = sum(x * y for x, y in zip(fila, col))  
+            nueva_fila.append(valor)
+        C.append(nueva_fila)
+    return C
+
+A = [[1, 2, 3],
+     [4, 5, 6]]
+
+B = [[7, 8],
+     [9, 10],
+     [11, 12]]
+
+for fila in mm(A, B):
+    print(fila)
+
+
---
+++ randompablovega.py
@@ -0,0 +1,12 @@
+import random
+
+def get_random_dice_roll():
+    random_number = random.randint(1, 20)
+    return random_number
+
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+
