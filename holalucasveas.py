af5f941f2e0db51d55b82e6582b9ede54a4e4479
lucasveas-dot <lucas.veas@userena.cl>
2025-10-20 00:46:22 -0300

    Add files via upload

---
+++ holalucasveas.py
@@ -0,0 +1,8 @@
+spam = int(input("Ingrese un valor numerico: "))
+
+if spam == 1:
+    print("Hola")
+elif spam == 2:
+    print("Hola")
+else:
+    print("¡Saludos!")
---
+++ randomlucasveas.py
@@ -0,0 +1,10 @@
+import random  # Importa el módulo random
+random_number = random.randint(1, 6) # Genera un número aleatorio entre 1 y 6
+
+def get_random_dice_roll():# Función para obtener un número aleatorio entre 1 y 6
+    return random_number 
+
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
+print(get_random_dice_roll())
