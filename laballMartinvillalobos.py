b785b0173b8548b2d7a923fbbedc9510a92b53dc
martinvillalobosg <martin.villalobosg@userena.cl>
2025-11-11 15:26:32 -0300

    Add files via upload

---
+++ laballMartinvillalobos.py
@@ -0,0 +1,48 @@
+
+laberinto = [
+    [0, 0, 1, 0, 0],  
+    [0, 0, 1, 0, 0],  
+    [0, 1, 0, 0, 0],  
+    [0, 0, 0, 1, 0],  
+    [1, 0, 0, 0, 0]
+]
+
+N = 5
+
+mov_x = [-1, 1, 0, 0]
+mov_y = [0, 0, -1, 1]
+
+soluciones = []
+
+def es_valido(x, y, lab, visitado):
+    return (
+        0 <= x < N and
+        0 <= y < N and
+        lab[x][y] == 0 and
+        not visitado[x][y]
+    )
+
+def resolver_todas(lab, x, y, fin_x, fin_y, visitado, camino):
+    if x == fin_x and y == fin_y:
+        soluciones.append(camino[:] + [(x, y)])
+        return
+
+    visitado[x][y] = True
+    camino.append((x, y))
+
+    for i in range(4):
+        nx = x + mov_x[i]
+        ny = y + mov_y[i]
+
+        if es_valido(nx, ny, lab, visitado):
+            resolver_todas(lab, nx, ny, fin_x, fin_y, visitado, camino)
+
+    visitado[x][y] = False
+    camino.pop()
+
+visitado = [[False]*N for _ in range(N)]
+resolver_todas(laberinto, 0, 0, 0, 4, visitado, [])
+
+print("Total soluciones:", len(soluciones))
+for sol in soluciones:
+    print(sol)
