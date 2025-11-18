0c2945b343a1b93f54a97c1420826ece907d4d4f
juliantapiaf <julian.tapiaf@userena.cl>
2025-11-18 18:24:43 -0300

    Update magicubejuliantapia.py

--- magicubejuliantapia.py
+++ magicubejuliantapia.py
@@ -1,25 +1,23 @@
-def all_combinaciones(lista):
-    n = len(lista)
-    lista_combinaciones = []
-    if n == 1:
-        return (lista)
-    elif n!=2:
-        fijado = lista[0]
-        lista.remove(fijado)
-        combinaciones_fijado = all_combinaciones(lista)
-        for i in range(len(combinaciones_fijado)):
-            combinacion = [fijado] + combinaciones_fijado[i]
-            lista_combinaciones.append(list(combinacion))
-            for j in range(len(combinacion)-1):
-                combinacion[j] = combinacion[j+1]
-                combinacion[j+1] = fijado
-                lista_combinaciones.append(list(combinacion))
-        return(lista_combinaciones)
+# Esta función es recursiva y permite calcular todas las permutaciones de una lista de elementos que se le entregue.
+def permutar(lista):
+    n = len(lista) # Numero de elementos de la lista
+    lista_permutaciones = [] # Lista vacia que guarda las permutaciones
+    if n == 1: # Condición de salida de la recursión. Si solo hay una elemento en la lista, se retorna la lista dentro de otra lista.
+        return [lista]
     else:
-        x = lista[0]; y = lista[1]
-        lista_combinaciones = [[x,y],[y,x]]
-        return(lista_combinaciones)
+        fijado = lista[0] # Se fija el primer elemento
+        lista.remove(fijado) # Se remueve el elemento fijado de la lista
+        permutaciones_fijado = permutar(lista) # Se calculan las permutaciones para los elementos de la lista sin el elemento fijado
+        for i in range(len(permutaciones_fijado)): # for que recorre los indices de la lista permutaciones.
+            permutacion = [fijado] + permutaciones_fijado[i] # Se define "permutacion", que agrega el fijado a la i-esima permutación.
+            lista_permutaciones.append(list(permutacion)) # Se agrega "permutacion" a la lista de permutaciones.
+            for j in range(len(permutacion)-1): # Este for mueve el fijado por la lista y agrega cada movimiento a la lista de permutaciones.
+                permutacion[j] = permutacion[j+1]
+                permutacion[j+1] = fijado
+                lista_permutaciones.append(list(permutacion))
+        return(lista_permutaciones) # Se retorna la lista de permutaciones para que sea ocupada por el proximo elemento fijado.
 
+# Esta función verifica si una permutación de la lista de permutaciones es un cuadrado magico o no y entrega los cuadrados magicos en una lista.
 def comprobar_CMs(lista):
     cuadrados_magicos = []
     for L in lista:
@@ -35,6 +33,7 @@ def comprobar_CMs(lista):
             cuadrados_magicos.append(L)
     return cuadrados_magicos
 
+# Esta función muestra los cuadrados magicos en la terminal.
 def mostrar_cuadrados(lista):
     for cuadrado in lista:
         print()
@@ -43,11 +42,11 @@ def mostrar_cuadrados(lista):
         print()
 
 # ------ MAIN
-lista_elementos = [i+1 for i in range(9)]
-combinaciones = all_combinaciones(lista_elementos)
-cuadrados_magicos = comprobar_CMs(combinaciones)
+lista_elementos = [i+1 for i in range(9)] # Los elementos del cuadrado magico, que van del 1 al 9.
+combinaciones = permutar(lista_elementos) # Se calculan las permutaciones
+cuadrados_magicos = comprobar_CMs(combinaciones) # Se determinan los cuadrados magicos
 print('¿Cuantos cuadrados magicos resultan al combinar los numeros del 1 al 9?')
-if cuadrados_magicos == []:
+if cuadrados_magicos == []: # Si la lista de cuadrados magicos estuviera vacia, no hay cuadrados magicos con los elementos que definimos.
     print('Ninguno.')
 else:
     mostrar_cuadrados(cuadrados_magicos)
