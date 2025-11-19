e455de822aab622d7b2aeedead6a71e84658ee6e
giovannieliantonio <giovanni.eliantonio@userena.cl>
2025-11-18 23:36:43 -0300

    magicubeGiovanniEliantonio.py

--- magicubeGiovanniEliantonio.py
+++ magicubeGiovanniEliantonio.py
@@ -1,14 +1,6 @@
 
-def imprimir_tablero(tablero):
-    # Imprime el tablero en formato de matriz
-    for i in range(tamaño_tablero):
-        for j in range(tamaño_tablero):
-            print(f'{tablero[i][j]:2}', end=" ")
-        print("")
-    print("")
-
 def cuadrado_magico(tablero):
-    # Calcula el valor que deberían tener todas las filas, columnas y diagonales
+    # Calcula la suma objetivo ("valor mágico") usando la primera fila
     valor_suma = 0
     for i in  range(tamaño_tablero):
         valor_suma += tablero[0][i] # suma la primera fila
@@ -48,7 +40,7 @@ def cuadrado_magico(tablero):
         suma += tablero[i][j]
         i += 1; j -= 1
 
-    # Si todo coincide, es un cuadrado mágico válido
+    # Si todas las verificaciones son correctas entonces es cuadrado mágico
     if suma != valor_suma:
         return False
     else:
@@ -60,6 +52,7 @@ def final(tablero):
         for j in range(tamaño_tablero):
             if tablero[i][j] == 0:
                 return False
+            
     return True
 
 def valida(tablero, candidato, i, j):
@@ -67,85 +60,61 @@ def valida(tablero, candidato, i, j):
     if tablero[i][j] != 0:
         return False
 
-    # # Revisar que el número no se haya usado antes
-    for i in range(tamaño_tablero):
-        for j in range(tamaño_tablero):
-            if tablero[i][j] == candidato:
+    """ Revisa que el número no se haya usado antes
+    El uso de x,y evita sobrescribir en i,j """
+    for x in range(tamaño_tablero):
+        for y in range(tamaño_tablero):
+            if tablero[x][y] == candidato:
                 return False
 
     return True
 
-def siguiente_posicion(i, j):
-    # Avanza a la siguiente casilla del tablero (recorrido por filas)
-    j += 1
-    if j == tamaño_tablero:
-        j = 0
-        i += 1
-    return i, j
-
-def posicion_anterior(i, j):
-    # Retrocede a la casilla anterior
-    j -= 1
-    if j < 0:
-        j = tamaño_tablero - 1
-        i -= 1
-    return i, j
-
-def encontrar_solucion(tablero):
+def encontrar_solucion(tablero, pos):
     
-    i, j = 0, 0         # Comenzamos en la casilla (0,0) con el primer número posible
-    candidato = 1
-    terminado = False
+    # Si se llenaron todas las casillas, se comprueba si es cuadrado mágico
+    if pos == tamaño_tablero * tamaño_tablero:
+        return cuadrado_magico(tablero)
 
-    while not terminado:
+    # Convertir "pos" a coordenadas (i,j), ej: pos 4: i = (4 // 3), j = (4 % 3) entones i = 1 y j = 0
+    # i: fila actual, j: columna actual
+    i = pos // tamaño_tablero
+    j = pos % tamaño_tablero
 
-        # # Probar todos los números posibles
-        while candidato <= tamaño_tablero * tamaño_tablero:
+    # Probar todos los números posibles en la casilla actual
+    for candidato in range(1, tamaño_tablero * tamaño_tablero + 1):
 
-            # Si el número se puede colocar aquí:
-            if valida(tablero, candidato, i, j):
-                tablero[i][j] = candidato  # colocarlo
+        if valida(tablero, candidato, i, j):
+            tablero[i][j] = candidato  # colocar candidato
 
-                # Si el tablero está lleno, verificar cuadrado mágico
-                if final(tablero):
-                    if cuadrado_magico(tablero):
-                        # Si lo es, ya terminamos
-                        terminado = True
-                        return True
-                    else:
-                        # # Si no lo es, se limpia la casilla y probar otro número
-                        tablero[i][j] = 0
-                        candidato += 1
-                        continue
+            # Llamada recursiva para avanzar a la siguiente casilla
+            if encontrar_solucion(tablero, pos + 1):  # recursión a siguiente casilla
+                return True
 
-                # Si no está lleno, avanzamos a la siguiente posición
-                i, j = siguiente_posicion(i, j)
-                candidato = 1   # reiniciar candidato en la nueva casilla
-                break  # salir para intentar en la nueva posición
+            # Si falló, retirar número
+            tablero[i][j] = 0
 
-            else:
-                candidato += 1 # Probar siguiente número
+    # Si ningún número funcionó, no hay solución
+    return False
 
-        else:  
-            # Si no quedan candidatos válidos, retrocedemos una casilla
-            if i == 0 and j == 0:
-                return False  # no existe solución
-
-            # volver atrás
-            i, j = posicion_anterior(i, j)
-            candidato = tablero[i][j] + 1   # probar siguiente número ahí
-            tablero[i][j] = 0   # limpiar casilla para deshacer paso del backtracking
+def mostrar_tablero(tablero):
+    """ Imprime el tablero en pantalla """
+    for i in range(tamaño_tablero):
+        for j in range(tamaño_tablero):
+            print(f'{tablero[i][j]:2}', end=" ")
+        print("")
+    print("")
 
 # Programa principal
 
-tamaño_tablero = 3     
+tamaño_tablero = 3
+# Crea un tablero vacío 
 tablero =[[0 for i in range(tamaño_tablero)] for j in range(tamaño_tablero)]
 
 print("Buscando solución...")
 
 # Ejecutar la búsqueda
-if encontrar_solucion(tablero):
+if encontrar_solucion(tablero, 0):
     print("\nSolución encontrada:")
-    imprimir_tablero(tablero)
+    mostrar_tablero(tablero)
 else:
     print("No existe una solucíon") 
